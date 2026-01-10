bac673aa086243de79b32a951d9e5526c5dcf833
Temitope <olawunitemitope68@gmail.com>
2026-01-10 09:19:06 +0100

    crud

--- app/(dashboard)/(rest)/workflows/page.tsx
+++ app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,9 +1,15 @@
+<<<<<<< Updated upstream
 import AppSidebar from '@/components/app-sidebar'
+=======
+import { WorkflowContainer, WorkflowList } from '@/features/workflow/components/workflows'
+import { prefetchWorkFlows } from '@/features/workflow/server/prefetch'
+>>>>>>> Stashed changes
 import { requireAuth } from '@/lib/auth-utils'
 import React from 'react'
 
 const page = async () => {
   await requireAuth()
+  prefetchWorkFlows()
   return (
    <div></div>
   )
